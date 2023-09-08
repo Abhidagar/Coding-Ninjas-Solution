57edbefdfd758f5e402de07bb0b08af4d8d138c7
Abhishek <abhidagar024@gmail.com>
2023-09-09 02:03:46 +0530

    Added a code to find power of a no

---
+++ find power of a number.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+    // Write your code here
+    int x, n;
+    cin >> x;
+    cin >> n;
+    int t = pow(x, n);
+    cout << t << endl;
+}
